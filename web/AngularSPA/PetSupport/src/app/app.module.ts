e6e5275a0b59c99b35f6b1318e7d51e49520fd25
ola-lola <olagrzegorczyk93@gmail.com>
2020-11-28 00:18:33 +0100

    add main-nav component

--- web/AngularSPA/PetSupport/src/app/app.module.ts
+++ web/AngularSPA/PetSupport/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatIconModule } from  '@angular/material/icon';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatListModule } from  '@angular/material/list';
+import { MatButtonModule } from  '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page/main-page.component';
 import { PettsitersComponent } from './petsitters/pettsiters/pettsiters.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    PettsitersComponent
+    PettsitersComponent,
+    MainNavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
