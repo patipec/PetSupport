630a169919fe26a9f47bc7dcf741903e0a00ce22
Lukasz Stanczak <lukasz.stanczak.mail@gmail.com>
2021-01-06 19:37:51 +0100

    Rename class PetsittersDTO.cs to PetsitterDTO.cs

--- backend/PetSupport.Infrastructure.Data/Data/DataContext.cs
+++ backend/PetSupport.Infrastructure.Data/Data/DataContext.cs
@@ -12,12 +12,11 @@ namespace PetSupport.Infrastructure.Data.Data
 
         }
         
-
-        private DbSet<Client> Clients { get; set; }
-        private DbSet<Petsitter> Petsitters { get; set; }
-        private DbSet<Service> Services { get; set; }
-        private DbSet<PetsitterService> PetSupportServices { get; set; }
-        private DbSet<BookingMessage> BookingMessages { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Petsitter> Petsitters { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<PetsitterService> PetSupportServices { get; set; }
+        public DbSet<BookingMessage> BookingMessages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
