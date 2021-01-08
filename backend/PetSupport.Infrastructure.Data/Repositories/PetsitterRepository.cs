efb2be0cdb888aed22fe5ac06f763f7cd855b743
patipec <ppeczynska@gmail.com>
2021-01-08 13:57:03 +0100

    PetsitterRepository improvements

--- backend/PetSupport.Infrastructure.Data/Repositories/PetsitterRepository.cs
+++ backend/PetSupport.Infrastructure.Data/Repositories/PetsitterRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PetSupport.Core.Entities;
 using PetSupport.Core.Interfaces;
 using PetSupport.Infrastructure.Data.Data;
@@ -9,35 +12,101 @@ using PetSupport.Infrastructure.Data.Data;
 namespace PetSupport.Infrastructure.Data.Repositories
 {
     public class PetsitterRepository : IRepository<Petsitter>, IPetsitterRepository
+
     {
-        public Task<Petsitter> GetByIdAsync(int id)
+        internal readonly DataContext Context;
+        private DbSet<Petsitter> Petsitters; 
+
+        public PetsitterRepository(DataContext context) 
+
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            context = Context;
+        }
+        
+
+        public  async Task<Petsitter> GetByIdAsync(int id)
         {
             throw new NotImplementedException();
+
         }
 
-        public Task<IEnumerable<Petsitter>> GetAllAsync()
+        public async Task<IEnumerable<Petsitter>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await Petsitters.ToListAsync();
         }
 
+        //Filters: string City, enum ServiceType Name, enum Unit Unit, doble Price
         public Task<IEnumerable<Petsitter>> FindByConditionAsync(Expression<Func<Petsitter, bool>> expression)
         {
             throw new NotImplementedException();
         }
+        //TODO: TEST below
+        public virtual async Task<IEnumerable<Petsitter>> GetFilteredAsync(
+            Expression<Func<Petsitter, bool>> filter = null,
+            Func<IQueryable<Petsitter>, IOrderedQueryable<Petsitter>> orderBy = null,
+            string includeProperties = "",
+            int first = 0, int offset = 0)
+        {
+            IQueryable<Petsitter> query = Petsitters;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (offset > 0)
+            {
+                query = query.Skip(offset);
+            }
+
+            if (first > 0)
+            {
+                query = query.Take(first);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+
 
         public void Add(Petsitter entity)
         {
-            throw new NotImplementedException();
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            Petsitters.Add(entity);
+            SaveChangesAsync();
         }
 
         public void Update(Petsitter entity)
         {
-            throw new NotImplementedException();
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            Petsitters.Update(entity);
+            SaveChangesAsync();
+
         }
 
         public Task<bool> SaveChangesAsync()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
+
+
     }
 }
--- backend/Petsupport.API2/Controllers/ServicesController.cs
+++ backend/Petsupport.API2/Controllers/ServicesController.cs
@@ -25,14 +25,14 @@ namespace PetSupport.API2.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Service>>> GetService()
         {
-            return await _context.Service.ToListAsync();
+            return await _context.Services.ToListAsync();
         }
 
         // GET: api/Services/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Service>> GetService(int id)
         {
-            var service = await _context.Service.FindAsync(id);
+            var service = await _context.Services.FindAsync(id);
 
             if (service == null)
             {
@@ -43,7 +43,7 @@ namespace PetSupport.API2.Controllers
         }
 
         // PUT: api/Services/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        
         [HttpPut("{id}")]
         public async Task<IActionResult> PutService(int id, Service service)
         {
@@ -78,7 +78,7 @@ namespace PetSupport.API2.Controllers
         [HttpPost]
         public async Task<ActionResult<Service>> PostService(Service service)
         {
-            _context.Service.Add(service);
+            _context.Services.Add(service);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetService", new { id = service.Id }, service);
@@ -88,13 +88,13 @@ namespace PetSupport.API2.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteService(int id)
         {
-            var service = await _context.Service.FindAsync(id);
+            var service = await _context.Services.FindAsync(id);
             if (service == null)
             {
                 return NotFound();
             }
 
-            _context.Service.Remove(service);
+            _context.Services.Remove(service);
             await _context.SaveChangesAsync();
 
             return NoContent();
@@ -102,7 +102,7 @@ namespace PetSupport.API2.Controllers
 
         private bool ServiceExists(int id)
         {
-            return _context.Service.Any(e => e.Id == id);
+            return _context.Services.Any(e => e.Id == id);
         }
     }
 }
