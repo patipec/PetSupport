51512955243e6b512ea1d6bdb7267b169510c431
Wojciech Jablonski <jablonski91w@gmail.com>
2021-01-21 22:01:32 +0100

    slider in progress

--- web/AngularSPA/PetSupport/src/app/pages/petsitters/petsitter/petsitter.component.ts
+++ web/AngularSPA/PetSupport/src/app/pages/petsitters/petsitter/petsitter.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {PetsittersService} from '../petsitters.service';
 import {Petsitter} from '../../../common/models/petsitter';
@@ -8,9 +8,10 @@ import {Petsitter} from '../../../common/models/petsitter';
   templateUrl: './petsitter.component.html',
   styleUrls: ['./petsitter.component.css']
 })
-export class PetsitterComponent implements OnInit {
+export class PetsitterComponent implements OnInit, AfterViewInit {
   private petsitterId: string;
   public petsitter: Petsitter;
+  private blockSlider = false;
   constructor(private route: ActivatedRoute, private petsittersService: PetsittersService) {
   }
 
@@ -18,6 +19,29 @@ export class PetsitterComponent implements OnInit {
     this.petsitterId = this.route.snapshot.paramMap.get('id') as string;
     console.log(this.petsitterId);
     this.petsittersService.getPetsitter(+this.petsitterId).subscribe( data => this.petsitter = data);
-  }
+    }
+  ngAfterViewInit(): void {
+    const arrowPrev = document.getElementsByClassName('carousel-arrow-prev');
+    console.log(arrowPrev);
+    arrowPrev[0].addEventListener('click', () => {
+      if (this.blockSlider === false){
+        this.blockSlider = !this.blockSlider;
+        new ElementRef(arrowPrev[1] as any).nativeElement.click();
+      } else{
+        this.blockSlider = !this.blockSlider;
+      }
+      console.log(this.blockSlider);
+    });
 
+    arrowPrev[1].addEventListener('click', () => {
+      if (this.blockSlider === false){
+        this.blockSlider = !this.blockSlider;
+        new ElementRef(arrowPrev[0] as any).nativeElement.click();
+      }else{
+        this.blockSlider = !this.blockSlider;
+      }
+
+      console.log(this.blockSlider);
+    });
+  }
 }
