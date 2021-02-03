59ed9cf6cbaf80f4c1b0dc3d48f7d7b6544bdd4b
Lukasz Stanczak <lukasz.stanczak.mail@gmail.com>
2021-02-03 19:37:43 +0100

    Add CoordinateDTO

---
+++ backend/Petsupport.API2/Dtos/OutDtos/CoordinateDTO.cs
@@ -0,0 +1,8 @@
+namespace Petsupport.API2.Dtos.OutDtos
+{
+    public class CoordinateDTO
+    {
+        public double Lattiude { get; set; }
+        public double Longtitude { get; set; }
+    }
+}
