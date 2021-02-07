ae4b090c57932eb6329e0d19f3545f9ab475bc1e
zdulak <dpzdulski@gmail.com>
2021-02-07 12:01:50 +0100

    After merge problems solved

--- backend/Petsupport.API2/Mapping/PetSupportProfile.cs
+++ backend/Petsupport.API2/Mapping/PetSupportProfile.cs
@@ -1,11 +1,11 @@
-using System.Linq;
 using AutoMapper;
 using Petsupport.API2.Dtos.InDtos;
 using Petsupport.API2.Dtos.OutDtos;
+using Petsupport.API2.Profiles;
 using PetSupport.Core.Entities;
 using User = Microsoft.Graph.User;
 
-namespace Petsupport.API2.Profiles
+namespace Petsupport.API2.Mapping
 {
     public class PetSupportProfile: Profile
     {
@@ -15,20 +15,12 @@ namespace Petsupport.API2.Profiles
                 .ForMember(dto => dto.ImageId, opt =>
                     opt.MapFrom(p => p.PhotoId))
                 .ReverseMap();
-<<<<<<< HEAD:backend/Petsupport.API2/Profiles/PetSupportProfile.cs
 
-=======
             
             this.CreateMap<Coordinate, CoordinateDTO>()
                 .ReverseMap();;
-            
-            
-            this.CreateMap<Petsitter, CreatePetsitterDTO>()
-                .ForMember(p => p.ImageId, o => o.MapFrom(m => m.PhotoId))
-                .ReverseMap();
-            
-            
->>>>>>> 9613a4fc8544779b6f923998e48f6c93e3b9aae0:backend/Petsupport.API2/Mapping/PetSupportProfile.cs
+
+
             this.CreateMap<Service, ServiceDTO>()
                 .ReverseMap();
             
