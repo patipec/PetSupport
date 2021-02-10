c128ff28555e961e5a52ab0f3b8fc4f0758eb187
Paweł Bednarczyk <bedpaw97@gmail.com>
2021-02-10 21:13:06 +0100

    quickfix for users who dont allow navigation api

--- web/AngularSPA/PetSupport/src/app/common/components/map/map.component.ts
+++ web/AngularSPA/PetSupport/src/app/common/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import {MapService} from './map.service';
 import {Petsitter} from '../../models/petsitter';
@@ -19,10 +19,7 @@ export class MapComponent implements OnChanges, OnInit {
   }
 
   public ngOnInit(): void {
-
     this.getUserCurrentPostion();
-
-    console.log(this.lng, this.lng);
   }
 
 
@@ -46,11 +43,23 @@ export class MapComponent implements OnChanges, OnInit {
 
   getUserCurrentPostion(): void {
     navigator.geolocation.getCurrentPosition((position) => {
-      this.lat = position.coords.latitude;
-      this.lng = position.coords.longitude;
-      this.initMap();
-
-    });
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.initMap();
+      },
+      () => {
+        /*        this.petsitterList
+                if (/!*this.petsitterList[0]?.coordinates[0]*!/ false) {
+                  this.lat = +this.petsitterList[0].coordinates[0].latitude;
+                  this.lng = +this.petsitterList[0].coordinates[0].longitude;
+                } else {*/
+        this.lat = 52.23;
+        this.lng = 21;
+        /*        }*/
+        this.initMap();
+      },
+      {timeout: 5000}
+    );
   }
 
   // public initPetsitterCords(): void{
