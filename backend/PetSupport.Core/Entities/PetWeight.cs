d5a143813574dbf2f7ae88ddd359bdb69694b762
Lukas-Stanleyy <lukasz.stanczak.mail@gmail.com>
2021-02-11 17:28:41 +0100

    Add new property to Service.cs and Petsitter.cs

---
+++ backend/PetSupport.Core/Entities/PetWeight.cs
@@ -0,0 +1,10 @@
+namespace PetSupport.Core.Entities
+{
+    public enum PetWeight
+    {
+        UpTo5kg = 0,
+        Between5And10kg,
+        Between10and25kg,
+        Above25kg
+    }
+}
--- backend/PetSupport.Core/Entities/Petsitter.cs
+++ backend/PetSupport.Core/Entities/Petsitter.cs
@@ -1,4 +1,4 @@
-﻿#nullable enable
+﻿
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,36 +9,26 @@ namespace PetSupport.Core.Entities
     public class Petsitter : User
     {
         
-        public string? Description { get; set; }
-        
-        public string? PhotoId { get; set; }
-
-        public string? City { get; set; }
-
-        public string? Street { get; set; }
-
-        public string? ParcelAndHouseNumbers { get; set; }
-
-        public string? ZipCode { get; set; }
-
-        public string? Country { get; set; }
+        public string Description { get; set; }
+        public string PhotoId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string ParcelAndHouseNumbers { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
         
         public double Rate { get; set; }
 
         public DateTime AvailabilityFrom { get; set; }
-
         public DateTime AvailabilityTo { get; set; }
-
         public DateTime DateOfBirth { get; set; }
-        
-        public string? Title { get; set; }
-
-        public string? Environment { get; set; }
+        public string Title { get; set; }
+        public string Environment { get; set; }
 
         public string Experience { get; set; }
-        public ICollection<Coordinate>? Coordinates { get; set; } 
         
-        public ICollection<Service>? Services { get; set; }
+        public ICollection<Coordinate> Coordinates { get; set; }
+        public ICollection<Service> Services { get; set; }
     }
 
     public class Coordinate : BaseEntity
