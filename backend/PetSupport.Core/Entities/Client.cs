bcbac3554002bac7bd4ebe128127e9d66cd60b74
Lukas-Stanleyy <lukasz.stanczak.mail@gmail.com>
2021-02-16 19:02:27 +0100

    Add ClientsController.cs and ClientRepository.cs, Add collection BookingMessage to Client.cs

--- backend/PetSupport.Core/Entities/Client.cs
+++ backend/PetSupport.Core/Entities/Client.cs
@@ -1,7 +1,9 @@
-﻿namespace PetSupport.Core.Entities
+﻿using System.Collections.Generic;
+
+namespace PetSupport.Core.Entities
 {
     public class Client : User
     {
-        
+        public ICollection<BookingMessage> BookingMessages { get; set; }
     }
 }
