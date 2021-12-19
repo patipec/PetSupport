1f13087e4356a50ad6ad61da6671e2e323a8d155
patipec <ppeczynska@gmail.com>
2021-12-19 13:44:49 +0100

    Delete In and Out DTOs, create Brief and PetsitterProfile DTO in DUPLEXDtos

---
+++ backend/Petsupport.API2/Dtos/DuplexDto/PetsitterBriefDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Petsupport.API2.Dtos.InDtos
+{
+    public class PetsitterBriefDtoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string AvatarId { get; set; }
+        public string City { get; set; }
+        public string ShortDescription { get; set; }
+    }
+}
--- backend/Petsupport.API2/Dtos/InDtos/CreateBookingMessageDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Petsupport.API2.Dtos.InDtos
-{
-    public class CreateBookingMessageDTO
-    {
-        public int ClientId { get; set; }
-        public int PetsitterId { get; set; }
-        public string Message { get; set; }
-    }
-}
